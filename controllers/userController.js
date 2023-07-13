63150a55a2c66bc2d8c35098011f1ee5b870cb75
Behzad Koulivand <behzadk.1245@gmail.com>
2023-07-13 18:40:11 +0330

    Uninstalling express-validator

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
 const bcript = require('bcryptjs');
-const {check, validationResult} = require('express-validator').check;
 
 
 const User = require('../models/User');
