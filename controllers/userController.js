0feb262ae00e99f458ae0b90a6a73f2441237611
Behzad Koulivand <behzadk.1245@gmail.com>
2023-07-13 18:59:28 +0330

    CreateUser in userController edited

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,5 +5,16 @@ const bcript = require('bcryptjs');
 const User = require('../models/User');
 
 exports.createUser = async (req, res) =>{
-    await User.create(req.body);
+    try {
+        const {email} = req.body;
+        await userValidation(req.body);
+        const user = await User.find({ email });
+        if(user) return res.status(400).json("کاربری با این ایمیل قبلا ثبت شده است");
+
+        await User.create(req.body);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json("خطایی وجود دارد");
+    }
+    
 }
