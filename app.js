0a26e50d7eaca5860bec15ef4b7089a1ffd98438
Behzad Koulivand <behzadk.1245@gmail.com>
2023-07-13 12:06:33 +0330

    Adding badyParser to app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ connectDB();
 
 const app = express();
 
+//Body Parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
